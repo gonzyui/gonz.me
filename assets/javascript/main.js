ebe699ad2fc0c2387a082ac593ab6655987b7a4a
Gonz <78351336+gonzyui@users.noreply.github.com>
2021-08-03 19:27:56 +0200

    Added 404 page, navbar and responsive

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.nav-bar-toggle').on('click', function() {
+        if ($('.nav-bar-links').hasClass('active')) {
+            $('.nav-bar-links').removeClass('active');
+        } else {
+            $('.nav-bar-links').addClass('active');
+        }
+    })
+});
