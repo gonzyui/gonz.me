07cdc48460dad35ba43f05783e4d277b33b1942c
gonzyui <tomarauch15@gmail.com>
2021-09-21 03:52:56 +0200

    recoded the whole site

--- assets/javascript/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.nav-bar-toggle').on('click', function() {
-        if ($('.nav-bar-links').hasClass('active')) {
-            $('.nav-bar-links').removeClass('active');
-        } else {
-            $('.nav-bar-links').addClass('active');
-        }
-    })
-});
